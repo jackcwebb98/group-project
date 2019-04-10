7034abd8085c6ef669ab66292afcb49eba6949f3
Jack Webb <jackwebb@Jacks-MacBook-Pro-2.local>
2019-04-10 14:47:07 -0600

    fixed app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 import { HashRouter } from "react-router-dom";
 import routes from './routes'
-import Profile from './components/ProfilePage'
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div className="App"><Profile /></div>
+        <div className="App">{routes}</div>
       </HashRouter>
     );
   }
