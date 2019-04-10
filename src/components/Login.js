264e4712e579429537b7cc54747f6514b0404848
Patrick Roach <patrickevanroach@gmail.com>
2019-04-10 14:35:00 -0600

    login routes push to register and landing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,39 @@
 import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
-import axios from 'axios'
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 
 
 const InputPassword = styled.input`
-position: relative;
-left: 30vw;
-top: 50vh;
+
 `
 const InputUsername = styled.input`
-position: relative;
-left: 20vw;
-top: 50vh;
+
 `
 
 const LoginButton = styled.button`
-position: relative;
-right: 8vw;
-top: 60vh;
 `
 
 const PasswordButton = styled.button`
-position: relative;
-right: 5vw;
-top: 60vh;
 `
 
 const LogoImg = styled.img`
-top: -10vh;
-right: 15vw;
+hieght: 300px;
+width: 80%;
+overflow: hidden;
 position: absolute;
-height: 450px;
-width: 450px;
+top: 10vh;
+`
+const LoginDiv = styled.div`
+display:flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: center;
+align-items: center;
+align-content: space-between;
+top:20vh;
+position:realative;
 `
 
 export default function Login(props){
@@ -40,8 +41,9 @@ export default function Login(props){
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [userId, setUserId] = useState('')
 
-    async function loginUser() {
+    async function loginUser(username,password) {
             let user = {
                 username: username,
                 password: password
@@ -49,7 +51,8 @@ export default function Login(props){
             
         try {
             let res = await axios.post('login', user);
-         props.updateUser(res.data)
+         
+         console.log(res.data.email)
          props.history.push('/landing')
         } catch(err) {
             alert('Incorrect username or password')
@@ -58,15 +61,20 @@ export default function Login(props){
 
 
     return(
-
-        <div>
+        <>
         <LogoImg src="https://trello-attachments.s3.amazonaws.com/5ca7c5be629a90869b43bcaa/500x500/edf9959df2fe8efd713e62192ad67d96/E-VAL-U-DATE_BL_BG.png"/>
+        <LoginDiv>
+            
+        
+        
         <InputUsername value={username} onChange={e => setUsername(e.target.value)}/>
         <InputPassword value={password} onChange={e => setPassword(e.target.value)}/>
-        <LoginButton onClick={loginUser}>Login</LoginButton>
+        <LoginButton onClick={()=>{loginUser(username, password)}}>Login</LoginButton>
+        <Link to={'/register'}>
         <PasswordButton>Register</PasswordButton>
-
-        </div>
+        </Link>
+        </LoginDiv>
+        </>
     )
 
 }
