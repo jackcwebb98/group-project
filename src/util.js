e504a3528301bbde1b0865088211bfb09b2178b4
Patrick Roach <patrickevanroach@gmail.com>
2019-04-23 12:13:10 -0600

    patrick typeof tests

--- src/util.js
+++ src/util.js
@@ -8,12 +8,36 @@ export async function checkUser(props) {
 }
 
 
-export  function findUser(name, lastName) {
+export function findUser(name, lastName) {
   let newUser = name + ' ' + lastName
   return newUser
 }
 
 export function addTogether(num1, num2) {
   let value = num1 + num2
+  return value
+}
+
+
+export function filteredSearch(e, users) {
+
+  let currentList = [];
+  let newList = [];
+
+  if (e !== "") {
+    currentList = users;
+    newList = currentList.filter(name => {
+      const lc = name.username.toLowerCase();
+      const username = e.toLowerCase();
+      return lc.includes(username);
+    });
+  } else {
+    newList = users;
+  }
+  return (newList);
+}
+
+export function subtractOne(num1){
+  let value = num1 -=1
   return value
 }
